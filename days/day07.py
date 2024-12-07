e21fdc623c226927931dad7be68829e6b94f8b31
Yoshi van den Akker <yoshivda@gmail.com>
2024-12-07 23:19:15 +0100

    Change day 7 to RTL

--- days/day07.py
+++ days/day07.py
@@ -9,24 +9,25 @@ def solve(data):
 
 def part_one(data):
     return sum(total
-               for total, nums in [(int(line.split(':')[0]), tuple(int(x) for x in line.split()[1:])) for line in data]
+               for total, nums in [(int(line.split(':')[0]), tuple(int(x) for x in line.split()[:0:-1])) for line in data]
                if can_sum_to(nums, total))
 
 
 def part_two(data):
     return sum(total
-               for total, nums in [(int(line.split(':')[0]), tuple(int(x) for x in line.split()[1:])) for line in data]
-               if can_sum_to(nums, total, 0, True))
+               for total, nums in [(int(line.split(':')[0]), tuple(int(x) for x in line.split()[:0:-1])) for line in data][::-1]
+               if can_sum_to(nums, total, True))
 
 
-def can_sum_to(nums, total, acc=0, pt2=False):
-    if acc > total:
+def can_sum_to(nums, total, pt2=False):
+    print(nums, total)
+    if total < 0:
         return False
-    if len(nums) == 0:
-        return acc == total
-    return (can_sum_to(nums[1:], total, acc + nums[0], pt2)
-            or can_sum_to(nums[1:], total, acc * nums[0], pt2)
-            or (pt2 and can_sum_to(nums[1:], total, int(str(acc) + str(nums[0])), pt2)))
+    if len(nums) == 1:
+        return nums[0] == total
+    return (can_sum_to(nums[1:], total - nums[0], pt2)
+            or (total % nums[0] == 0 and can_sum_to(nums[1:], total // nums[0], pt2))
+            or (pt2 and str(total).endswith(str(nums[0])) and can_sum_to(nums[1:], int(str(total)[:-len(str(nums[0]))] or '0'), pt2)))
 
 
 if __name__ == '__main__':
